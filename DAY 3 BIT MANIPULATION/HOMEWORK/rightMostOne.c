e894b443f7ea70fb2f0d64a5bf514f1d48f0d62f
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 17:39:29 +0530

    Update rightMostOne.c

--- DAY 3 BIT MANIPULATION/HOMEWORK/rightMostOne.c
+++ DAY 3 BIT MANIPULATION/HOMEWORK/rightMostOne.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int rightMostOne(int N)
+{
+  return N & (~N+1);
+}
+int rightMostOnePosition(int N)
+{
+   int pos = 0;
+    while( (N&1) ==0 ){
+        N >>= 1;
+        pos++;
+    }
+    return pos+1;
+}
+int main()
+{
+  int N;
+  scanf("%d",&N);
+  printf("%d",rightMostOne(N));
+  printf("%d",rightMostOnePosition(N));
+  return 0;
+}
