499241bd4b9b948ba72b5017b8eff818c6c29cff
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 17:19:45 +0530

    Update powerOf2.cpp

--- DAY 3 BIT MANIPULATION/HOMEWORK/powerOf2.cpp
+++ DAY 3 BIT MANIPULATION/HOMEWORK/powerOf2.cpp
@@ -1,1 +1,11 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+bool powerOf2(int N)
+{
+  return N>0 && (N&(N-1))==0;
+}
+int main()
+{
+  int N;
+  cin>>N;
+  cout<<(powerOf2(N) ? "True" : "False")<<endl;
