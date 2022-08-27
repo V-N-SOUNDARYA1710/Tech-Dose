bd230326d4da0c80fac37b4bc83b87ae9648a9b2
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 17:24:00 +0530

    Update isSetBit.cpp

--- DAY 3 BIT MANIPULATION/HOMEWORK/isSetBit.cpp
+++ DAY 3 BIT MANIPULATION/HOMEWORK/isSetBit.cpp
@@ -1,1 +1,12 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+bool isSetBit(int N,int pos)
+{
+  return (N&(1<<(pos-1)))!=0;
+}
+int main()
+{
+  int N,pos;
+  cin>>N>>pos;
+  cout<<isSetBit(N,pos)<<endl;
+}
