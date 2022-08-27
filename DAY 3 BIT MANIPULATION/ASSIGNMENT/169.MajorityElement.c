325dfd051ed25f17b68b704dc9a6d289479df513
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 20:32:56 +0530

    Update 169.MajorityElement.c

--- DAY 3 BIT MANIPULATION/ASSIGNMENT/169.MajorityElement.c
+++ DAY 3 BIT MANIPULATION/ASSIGNMENT/169.MajorityElement.c
@@ -1,1 +1,13 @@
-
+int majorityElement(int* nums, int numsSize){
+    int count=0,majority=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(count==0)
+            return nums[i];
+        if(nums[i]==majority)
+            count++;
+        else
+            count--;
+    }
+    return count;
+}
