9e2b59f8cf34b0249b9a37ee7727f3cbbc8cd339
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 17:44:11 +0530

    Update compilement.c

--- DAY 3 BIT MANIPULATION/HOMEWORK/compilement.c
+++ DAY 3 BIT MANIPULATION/HOMEWORK/compilement.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+int Compliment1(int N)
+{
+  return ~N;
+}
+int Compliment2(int N)
+{
+  return ~N+1;
+}
+int main()
+{
+  int N;
+  scanf("%d",&N);
+  printf("%d",Compliment1(N));
+  printf("%d",Compliment2(N));
+  return 0;
+}
