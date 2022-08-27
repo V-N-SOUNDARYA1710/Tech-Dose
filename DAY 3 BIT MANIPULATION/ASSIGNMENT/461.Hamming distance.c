1e3e5a8b04ac09bbeda6613dae84b596bba07ee7
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 20:58:39 +0530

    Update 461.Hamming distance.c

--- DAY 3 BIT MANIPULATION/ASSIGNMENT/461.Hamming distance.c
+++ DAY 3 BIT MANIPULATION/ASSIGNMENT/461.Hamming distance.c
@@ -1,1 +1,11 @@
-
+int hammingDistance(int x, int y){
+    int dist = 0;
+    
+    while (x || y) {
+        if ((x&1) != (y&1))
+            dist++;
+        x >>= 1;
+        y >>= 1;
+    }
+    return dist;
+}
