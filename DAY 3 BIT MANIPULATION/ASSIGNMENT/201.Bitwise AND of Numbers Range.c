75ff82e49398b72e0be8c36c596ac843f820bcff
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 20:52:42 +0530

    Update 201.Bitwise AND of Numbers Range.c

--- DAY 3 BIT MANIPULATION/ASSIGNMENT/201.Bitwise AND of Numbers Range.c
+++ DAY 3 BIT MANIPULATION/ASSIGNMENT/201.Bitwise AND of Numbers Range.c
@@ -1,1 +1,14 @@
+int rangeBitwiseAnd(int m, int n) {
+    if (m >= n) {
+      return m;
+    }
 
+    int i = 0;
+    while (m != n) {
+      m /= 2;
+      n /= 2;
+      i ++;
+    }
+
+    return m << i;
+}
