6c8b7d6f2da1dacdd55e92b4c0b660adab2b4a2c
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 20:20:27 +0530

    Update 137.SingleNumberII.c

--- DAY 3 BIT MANIPULATION/ASSIGNMENT/137.SingleNumberII.c
+++ DAY 3 BIT MANIPULATION/ASSIGNMENT/137.SingleNumberII.c
@@ -1,1 +1,10 @@
-
+int singleNumber(int* nums, int numsSize){
+    int fir=0;
+    int sec=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        fir=fir^nums[i] & ~sec;
+        sec=sec^nums[i] & ~fir;
+    }
+    return fir;
+}
