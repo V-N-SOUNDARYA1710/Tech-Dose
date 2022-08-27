2560d1ecbe1222a5e0be099f2fa2a033d5ae973b
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 17:50:41 +0530

    Update countSetBit.c

--- DAY 3 BIT MANIPULATION/HOMEWORK/countSetBit.c
+++ DAY 3 BIT MANIPULATION/HOMEWORK/countSetBit.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+int countSetBit(int N)
+{
+  int ctr=0;
+  for(int i=1;i<N;i++)
+  {
+    if(N&i)
+    {
+      ctr++;
+    }
+  }
+  return ctr;
+}
+int main()
+{
+  int N;
+  scanf("%d",&N);
+  printf("%d",countSetBit(N));
+  return 0;
+}
