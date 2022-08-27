ed3994403dd61ae3240527ed120380e134019558
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 19:51:42 +0530

    Update 136.SingleNumber.c

--- DAY 3 BIT MANIPULATION/ASSIGNMENT/136.SingleNumber.c
+++ DAY 3 BIT MANIPULATION/ASSIGNMENT/136.SingleNumber.c
@@ -1,1 +1,10 @@
-
+int singleNumber(int* nums, int numsSize){
+    if(numsSize==0)
+        return nums[0];
+    int ctr=nums[0];
+    for(int i=1;i<numsSize;i++)
+    {
+        ctr^=nums[i];
+    }
+    return ctr;
+}
