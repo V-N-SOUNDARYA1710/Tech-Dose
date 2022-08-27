fe7b61bc42775b96fec0518e74801566d04c9013
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 18:51:20 +0530

    Update unsetithBit.c

--- DAY 3 BIT MANIPULATION/HOMEWORK/unsetithBit.c
+++ DAY 3 BIT MANIPULATION/HOMEWORK/unsetithBit.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int unsetIthBit(int N,int pos)
+{
+  return N & (~(1<<(pos-1)));
+}
+int main()
+{
+  int N,pos;
+  scanf("%d%d",&N,&pos);
+  printf("%d",unsetIthBit(N,pos));
+  return 0;
+}
