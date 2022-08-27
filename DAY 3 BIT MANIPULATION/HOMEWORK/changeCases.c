9f2bfa1b291d5772bea1acb007826e33b703eb15
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 18:16:22 +0530

    Update changeCases.c

--- DAY 3 BIT MANIPULATION/HOMEWORK/changeCases.c
+++ DAY 3 BIT MANIPULATION/HOMEWORK/changeCases.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+char toLower(char ch){
+    return ch | (1<<5);
+}
+char toUpper(char ch){
+    return ch & ( ~(1<<5) );
+}
+char toToggle(char ch){
+    return ch ^ (1<<5);
+}
+int main(){
+  char ch;
+  scanf("%c",&ch);
+  printf("%c",toLower(ch));
+  printf("%c",toUpper(ch));
+  printf("%c",toToogle(ch));
+  return 0;
+}
