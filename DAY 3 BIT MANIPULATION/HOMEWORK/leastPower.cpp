806d2834ad0876c2773c63c0ae97ee5ae3e151b1
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 17:31:09 +0530

    Update leastPower.cpp

--- DAY 3 BIT MANIPULATION/HOMEWORK/leastPower.cpp
+++ DAY 3 BIT MANIPULATION/HOMEWORK/leastPower.cpp
@@ -1,5 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int 
+int leastPower(int N)
+{
+  int ctr=0;
+  while(N && N&(N-1))
+  {
+    N=N&(N-1);
+  }
+  return N;
+}
 int main()
 {
+  int N;
+  cin>>N;
+  cout<<leastPower(N)<<endl;
+  return 0;
+}
