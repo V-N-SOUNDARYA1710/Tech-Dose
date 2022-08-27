1e561839379235c15123e11c445b683732897706
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-08-27 20:56:16 +0530

    Update 338.Counting Bits.c

--- DAY 3 BIT MANIPULATION/ASSIGNMENT/338.Counting Bits.c
+++ DAY 3 BIT MANIPULATION/ASSIGNMENT/338.Counting Bits.c
@@ -1,1 +1,8 @@
+int* countBits(int n, int* returnSize){
+    int *res = malloc(sizeof(int) * (n+1));
+    for (int i = 0; i <= n; i++)
+        res[i] = __builtin_popcount(i);
+    *returnSize = n+1;
+    return res;
 
+}
