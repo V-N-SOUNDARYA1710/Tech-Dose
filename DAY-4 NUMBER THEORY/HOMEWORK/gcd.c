58f4a15e44a9370adeff0b9d87c2f367d2c56873
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-09-06 19:59:36 +0530

    Update gcd.c

--- DAY-4 NUMBER THEORY/HOMEWORK/gcd.c
+++ DAY-4 NUMBER THEORY/HOMEWORK/gcd.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int gcd(int a,int b)
+{
+  if(b==0)
+  {
+    return a;
+  }
+  else
+  {
+    return gcd(a%b,b);
+  }
+}
+int main()
+{
+  int a,b;
+  scanf("%d%d",&a,&b);
+  printf("%d",gcd(a,b));
+}
