6dfbec5a8cb67f8f626ca87646f0710641b0a5f8
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-09-06 19:56:50 +0530

    Update catalanNumber.cpp

--- DAY-4 NUMBER THEORY/HOMEWORK/catalanNumber.cpp
+++ DAY-4 NUMBER THEORY/HOMEWORK/catalanNumber.cpp
@@ -1,1 +1,24 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> catalan;
+int getCatalan(int N)
+{
+  if(N==0 || N==1)
+    return 1;
+  if(catalan[N]!=-1)
+    return catalan[N];
+  int currCatalan=0;
+  for(int i=0;i<N;i++)
+  {
+    currCatalan+=getcatalan(i)*getCatalan(N-i-1);
+  }
+  return catalan[N]=currCatalan;
+}
+int main()
+{
+  int N;
+  cin>>N;
+  catalan.resize(N+1,-1);
+  cout<<getCatalan(N)<<endl;
+  return 0;
+}
