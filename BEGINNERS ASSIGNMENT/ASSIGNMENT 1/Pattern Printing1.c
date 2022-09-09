3da221a723a50b7a8d9968200253f9a7647d549d
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-09-09 11:48:49 +0530

    Update Pattern Printing1.c

--- BEGINNERS ASSIGNMENT/ASSIGNMENT 1/Pattern Printing1.c
+++ BEGINNERS ASSIGNMENT/ASSIGNMENT 1/Pattern Printing1.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main()
+{
+    int N=5;
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+}
