b3077aa1d61a4df6f52ba8cc216610009b6e0e18
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-09-09 11:43:28 +0530

    Create Area of a Circle.c

---
+++ BEGINNERS ASSIGNMENT/ASSIGNMENT 1/Area of a Circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int radius;
+    float PI=3.14;
+    scanf("%d",&radius);
+    float area=PI*radius*radius;
+    printf("The area of a circle is %.2f",area);
+}
