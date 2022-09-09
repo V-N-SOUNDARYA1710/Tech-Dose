851e260db09dc528bc9cb5d3da293e319d8d4faa
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-09-09 10:57:27 +0530

    Update Second Maximum in Array.c

--- BEGINNERS ASSIGNMENT/ASSIGNMENT 1/Second Maximum in Array.c
+++ BEGINNERS ASSIGNMENT/ASSIGNMENT 1/Second Maximum in Array.c
@@ -1,1 +1,28 @@
-
+#include<stdio.h>
+#include<limits.h>
+#define MAX_SIZE 1000
+int main()
+{
+    int arr[MAX_SIZE],first,second;
+    int N;
+    scanf("%d",&N);
+    for(int i=0;i<N;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    first=second=INT_MIN;
+    for(int i=0;i<N;i++)
+    {
+        if(arr[i]>first)
+        {
+            second=first;
+            first=arr[i];
+        }
+        else if(arr[i]>second && arr[i]<first)
+        {
+            second=arr[i];
+        }
+    }
+    printf("The second largest element in an array is %d",second);
+    return 0;
+}
