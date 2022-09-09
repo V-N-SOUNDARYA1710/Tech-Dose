6b6cd02bb58f3a679f1d761c469e9ec36be6d544
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-09-09 14:45:00 +0530

    Update Pattern Printing2.c

--- BEGINNERS ASSIGNMENT/ASSIGNMENT 1/Pattern Printing2.c
+++ BEGINNERS ASSIGNMENT/ASSIGNMENT 1/Pattern Printing2.c
@@ -5,10 +5,17 @@ int main()
   scanf("%d",&N);
   for(int i=1;i<=N;i++)
   {
-    for(int j=N;j>=1;j--)
+    for(int j=N;j>=i;j--)
     {
       printf("%d ",j);
     }
+    printf("\n");
   }
   return 0;
 }
+/*
+5 4 3 2 1
+5 4 3 2
+5 4 3
+5 4
+5*/
