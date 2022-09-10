1f99b66529dce103c144347d37df6e3bee6c2678
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-09-10 12:54:54 +0530

    Update and rename INSERTIONSORT.c to INSERTIONSORT.java

---
+++ SORTING/INSERTIONSORT.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    int arr[]=new int[N];
+    for(int i=0;i<N;i++)
+    {
+      arr[i]=sc.nextInt();
+    }
