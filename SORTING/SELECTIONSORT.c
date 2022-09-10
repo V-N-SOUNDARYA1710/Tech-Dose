5be02b38b06502514706195755a509afc55290a8
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-09-10 12:02:47 +0530

    Update SELECTIONSORT.c

--- SORTING/SELECTIONSORT.c
+++ SORTING/SELECTIONSORT.c
@@ -1,1 +1,29 @@
-
+#include<stdio.h>
+int main()
+{
+  int N;
+  scanf("%d",&N);
+  int arr[N];
+  for(int i=0;i<N;i++)
+    {
+      scanf("%d",&arr[i]);
+    }
+  for(int i=0;i<N-1;i++)
+    {
+      int ind=i;
+      for(int j=i+1;j<N;j++)
+        {
+          if(arr[ind]>arr[j])
+          {
+            ind=j;
+          }
+        }
+      int temp=arr[ind];
+      arr[ind]=arr[i];
+      arr[i]=temp;
+    }
+  for(int i=0;i<N;i++)
+    {
+      printf("%d ",arr[i]);
+    }
+}
