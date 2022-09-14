09f61a5927241f689e0e9c03a2f4a851c0d4a177
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-09-14 18:20:35 +0530

    Create 18.Print all digits of a given number.c

---
+++ BEGINNERS ASSIGNMENT/ASSIGNMENT 1/18.Print all digits of a given number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void printDigit(int N)
+{
+	int arr[MAX];
+	int i = 0;
+	int j, r;
+	while (N != 0) {
+		r = N % 10;
+		arr[i] = r;
+		i++;
+		N = N / 10;
+	}
+	for (j = i - 1; j > -1; j--) {
+		printf("%d ", arr[j]);
+	}
+}
+int main()
+{
+	int N ;
+  scanf("%d",&N);
+	printDigit(N);
+	return 0;
+}
