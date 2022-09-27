a9a2aa8ec7541865e4a4b0750308831a4f2daa3c
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-09-27 11:06:43 +0530

    Create 56.MERGE INTERVALS.java

---
+++ ARRAY/ASSIGNMENT/56.MERGE INTERVALS.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> (a[0] - b[0]));
+        ArrayList<int []> result = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            if (i == 0) {
+                result.add(intervals[0]); 
+                continue;
+            }
+            int[] prev = result.get(result.size()-1);
+            int[] curr = intervals[i];
+            if (prev[1] >= curr[0]) {
+                prev[1] = Math.max(prev[1], curr[1]);
+            } else {
+                result.add(curr);
+            }
+        }
+        return result.toArray(new int[result.size()][2]);
+    }
+}
