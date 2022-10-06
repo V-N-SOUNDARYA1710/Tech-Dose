681752107fe37aebef037107a915a3b4b513b061
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-10-06 10:39:40 +0530

    Update 907.Sum of Subarray Minimums.c

--- ARRAY/ASSIGNMENT/907.Sum of Subarray Minimums.c
+++ ARRAY/ASSIGNMENT/907.Sum of Subarray Minimums.c
@@ -41,3 +41,25 @@ int sumSubarrayMins(int* arr, int arrSize){
     
     return sum;
 }
+/*
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        Stack<Integer> s=new Stack<>();
+        s.push(-1);
+        long res=0;
+        for(int i=0;i<arr.length+1;i++){
+            int currVal=(i<arr.length)?arr[i]:0;
+            while(s.peek()!=-1 && currVal<arr[s.peek()]){ // Monotone Icreasing Stack
+                int ind=s.pop();
+                int j=s.peek();
+                int l=ind-j;
+                int r=i-ind;
+                long ans=(long)l*r*arr[ind];
+                res+=ans;
+                res%=1000000007;
+            }
+            s.push(i);
+        }
+        return (int)res;
+    }
+}*/
