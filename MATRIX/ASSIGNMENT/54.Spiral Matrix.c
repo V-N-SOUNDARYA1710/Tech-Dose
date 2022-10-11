071b0777ced6c7964a272d84398d0f62d2ca64be
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-10-11 12:05:03 +0530

    Update 54.Spiral Matrix.c

--- MATRIX/ASSIGNMENT/54.Spiral Matrix.c
+++ MATRIX/ASSIGNMENT/54.Spiral Matrix.c
@@ -1,1 +1,27 @@
-
+int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize)
+{
+    int * result = (int *)malloc(matrixSize*(*matrixColSize)*sizeof(int));
+    int left = 0, right = *matrixColSize-1, up = 0, down = matrixSize-1, index = 0;
+    while(left <= right && up <= down)
+    {
+        for(int i=left; i<=right; i++)
+        {
+            result[index++] = matrix[up][i];
+        }
+        for(int i=up+1; i<=down; i++)
+        {
+            result[index++] = matrix[i][right]; 
+        }
+        for(int i=right-1; i>left && up != down; i--)
+        {
+            result[index++] = matrix[down][i];
+        }
+        for(int i=down; i>up && left != right; i--)
+        {
+            result[index++] = matrix[i][left];
+        }
+        up++; down--; left++; right--;
+    }
+    *returnSize = index;
+    return result;
+}
