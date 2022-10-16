128d53bddb8e32c2c17a1c8ca40e9373b8b3358b
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-10-16 06:38:12 +0530

    Create 1672. Richest Customer Wealth.c

---
+++ LeetCode Contest/Contest 217/1672. Richest Customer Wealth.c
@@ -0,0 +1,13 @@
+int maximumWealth(int** accounts, int accountsSize, int* accountsColSize)
+{
+    int max=0;
+    for(int i=0;i<accountsSize;i++){
+        int sum=0;
+        for(int j=0;j<*accountsColSize;j++){
+            sum =sum+ accounts[i][j];
+        }
+        if(sum>max)
+            max=sum;
+    }
+    return max;
+}
