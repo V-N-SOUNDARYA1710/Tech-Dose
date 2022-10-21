01bcc9089f29c72467a3ca96666d4f84ccd2f376
V-N-SOUNDARYA1710 <112146873+V-N-SOUNDARYA1710@users.noreply.github.com>
2022-10-21 09:45:51 +0530

    Create 977. Squares of a Sorted Array.c

---
+++ LeetCode Contest/Contest 120/977. Squares of a Sorted Array.c
@@ -0,0 +1,14 @@
+int cmp (const void *a, const void *b) 
+{
+    return (*(int *)a - *(int *)b);
+}
+int* sortedSquares(int* nums, int numsSize, int* returnSize)
+{
+    *returnSize = numsSize;
+    for (int i=0; i<numsSize; i++) 
+    {
+        nums[i] = nums[i]*nums[i];
+    }
+    qsort (nums, numsSize, sizeof(int), cmp);
+    return nums;
+}
